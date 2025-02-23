050081160651c88aa5bac5e128d43faf0ecbb3ce
elizabeth-agada <elizadave6@gmail.com>
2025-02-23 19:45:24 +0100

    browser route fixed

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ProgressProvider } from "./context/ProgressContext"; // Import ProgressProvider
+import { ProgressProvider } from "./context/ProgressContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ProgressProvider>
     <BrowserRouter> 
-    <ProgressProvider> {/* Wrap the app with ProgressProvider */}
       <App />
-      </ProgressProvider>
     </BrowserRouter>
+    </ProgressProvider>
   </React.StrictMode>
 );
 
